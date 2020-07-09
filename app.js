d678d2677ea425e1e1b7c1ff47ad525d91de6b7b
N0mercy00 <dkdhehesp@naver.com>
2020-07-09 16:20:01 +0900

    add) function draw line on my little canvas

--- app.js
+++ app.js
@@ -1,13 +1,63 @@
 const canvas =document.getElementById("jsCanvas");
 /*캔버스를 먼저 가져와 준다 */
+const ctx= canvas.getContext("2d");
+/*캔버스 내부 픽셀들을 다루기 위해서 context 선언
+context는 캔버스 내부에서 픽셀들을 다루기 위한 것들*/
+
+canvas.width=700;
+canvas.height=700;
+/*pixel modifier 선언 얘 없으면 작동안해*/
+
+ctx.strokeStyle="#2c2c2c";
+/*캔버스 내부 색을 칠하기 위해 사용되는 strokeStyle*/
+ctx.lineWidth=2.5;
+/*붓 두께 */
+
 let painting =false;
 /*현재 그림 그리는중인지(클릭중인지) 파악하는 변수*/
 
-function onMouseMove(event){/*캔버스가 있는 경우 실행된다, 캔버스내의 좌표값인 offset을 가져온다*/
+
+function startPainting(){
+    painting=true;
+}
+
+function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
-    
+    /*캔버스가 있는 경우 실행된다, 캔버스내의 좌표값인 offset을 가져온다*/
+
+    if(!painting){
+        ctx.beginPath();/*사용할 path 생성*/
+        ctx.moveTo(x,y);/*시작점 기준*/
+        /*페인팅 비활성화시 페인ㅇ팅을 위한 path 사전생성?*/
+    }else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+        /*lineTo = 이전 위치의 path와 현위치를 이어서 path 만든다
+        stroke 색 칠한다 (그린다)
+        */
+    }  
 }
+/*canvas는 html5의 한 요소 캔버스 내부안에 있는 픽셀을 다룰수 있다.
+사용하기 위해서는 먼저 css와 실제 픽셀을 다루는 윈도우가 어느 사이즈인지 각각 width height 를 줘야한다.
+또한 내부의 픽셀을 다루기 위하여 context도 가져야 한다.
+ctx.strokeStyle은 그릴 선이 갖는 색을 지정해준다.
+
+클릭하지 않고 캔버스위에서 마우스를 움직이는 경우 path를 시작한다. path란 선이다.
+그렇게 만든 선 (path)를 마우스의 x,y 좌표로 이동시키는것. 즉 클릭을 안하고 마우스를 움직이는 상태에도 
+눈에 보이지 않을뿐  마우스를 따라 path가 만들어지고 있다.
+클릭을 하게 되면 페인팅 값이 참이 되고 클릭하기 직전의 위치에서 클릭을 유지하는 동안의 마우스 위치까지 
+색이 칠해진다.
+
+선을 그린다는것 = 시작 좌표에서 종료 좌표까지 색을 칠한다는것.
+시작점은 클릭하기 바로 직전(클릭하려는 위치와 동일) moveTO로 x,y 좌표가 지정되고
+클릭하는 순간 (painting=true) 현재 마우스의 좌표까지 색이 칠해진다
+*/
+
+
+
+
+
 function onMouseDown(event){
     painting =true;
     /*클릭시 활성화*/
@@ -18,7 +68,6 @@ function stopPainting(){
     /*페인팅 상대 중지*/
 }
 
-
 function onMouseUp(event){
     stopPainting();
     /*손때면 다시 비활성화*/
@@ -27,10 +76,10 @@ function onMouseUp(event){
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     /*캔버스가 있는지 확인 있다면 해당 함수 실행*/
-    canvas.addEventListener("mousedown",onMouseDown);
+    canvas.addEventListener("mousedown",startPainting);
     /*마우스 클릭시 발생하는 이벤트 추가*/
-    canvas.addEventListener("mouseup",onMouseUp);
+    canvas.addEventListener("mouseup",stopPainting);
     /*마우스에서 손때면 그림 그려지고있는거 활성화 종료해야하니*/
-    canvass.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
     /*작업도중 커서가 캔버스 밖으로 나갈경우 역시 활성화 중지*/
 }
